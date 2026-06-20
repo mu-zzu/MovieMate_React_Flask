dd34770f2886e7a514ea48a76edec53d7de1fbac
Muzaid Musthafa <muzzumusthafa@gmail.com>
2026-06-20 12:34:11 +0530

    Set up React Router with home and add movie pages

---
+++ MovieMate/frontend/src/pages/AddMovie.jsx
@@ -0,0 +1,10 @@
+function AddMovie() {
+    return (
+        <div>
+            <h1>Add New Movie</h1>
+            <p>Add movie form will be here.</p>
+        </div>
+    );
+}
+
+export default AddMovie;
---
+++ MovieMate/frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>MovieMate Dashboard</h1>
+            <p>Your movie collection will appear here.</p>
+        </div>
+    );
+}
+
+export default Home;
