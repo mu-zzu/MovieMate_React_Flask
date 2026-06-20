7f45798f6a43a9371590f1255a477b239666f48e
Muzaid Musthafa <muzzumusthafa@gmail.com>
2026-06-20 15:31:07 +0530

    Created navbar and React application layout

--- MovieMate/frontend/src/App.jsx
+++ MovieMate/frontend/src/App.jsx
@@ -2,22 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AddMovie from "./pages/AddMovie";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <BrowserRouter>
+            <Navbar />
+
             <Routes>
-
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
-
-                <Route
+                <Route path="/" element={<Home />} />
+                <Route 
                     path="/add"
                     element={<AddMovie />}
                 />
-
             </Routes>
         </BrowserRouter>
     );
